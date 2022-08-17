cd6c48729882b8b64f88889969883bdd790fd75c
melkml <mmldn7@gmail.com>
2022-08-17 10:21:48 -0300

    interface: invertido as peças.

--- chess-engine/libs/const.maps.ts
+++ chess-engine/libs/const.maps.ts
@@ -21,18 +21,18 @@ export const Pieces: Record<string, number> = {
   
 export const printPieces: Record<number, string> = {
     0: "-",
-    1: "♙",
-    2: "♘",
-    3: "♗",
-    4: "♖",
-    5: "♕",
-    6: "♔",
-    7: "♟",
-    8: "♞",
-    9: "♝",
-    10: "♜",
-    11: "♛",
-    12: "♚",
+    1: "♟",
+    2: "♞",
+    3: "♝",
+    4: "♜",
+    5: "♛",
+    6: "♚",
+    7: "♙",
+    8: "♘",
+    9: "♗",
+    10: "♖",
+    11: "♕",
+    12: "♔",
   };
   
 export const Positions: Record<string, [number, number]> = {
